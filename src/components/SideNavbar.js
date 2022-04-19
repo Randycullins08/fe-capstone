eb20af3bc434aae9db33a120b17bfb01fe0c809f
Randy Cullins <randycullins2018@gmail.com>
2022-04-18 19:57:12 -0600

    first commit

---
+++ src/components/SideNavbar.js
@@ -0,0 +1,7 @@
+export default function SideNavbar() {
+  return (
+    <div>
+      <h1>Hello From Side navbar</h1>
+    </div>
+  );
+}
---
+++ src/components/TopNavbar.js
@@ -0,0 +1,7 @@
+export default function TopNavbar() {
+  return (
+    <div className="top-navbar-container">
+      <h1>Hello from top navbar</h1>
+    </div>
+  );
+}
